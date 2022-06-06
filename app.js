761a3292d8b85242468cfdfa8465280bd07c63ba
Valeriya Uldanova <uv_hist@mail.ru>
2022-06-07 01:40:59 +0300

    123

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 
-const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { userRouter } = require('./routes/users');
 const { cardRouter } = require('./routes/cards');
+
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
