a83c18e919f04b5a75907b01dd9548af15791351
Gormsdottir <uv_hist@mail.ru>
2022-06-08 22:41:27 +0300

    commit

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 
 const {
   getUsers,
-  getUser,
+  getUserById,
   createUser,
   updateUser,
   updateAvatar,
 } = require('../controllers/users');
 
-router.get('/users', getUsers);
-router.get('/users/:userId', getUser);
-router.post('/users', createUser);
-router.patch('/users/me', updateUser);
-router.patch('/users/me/avatar', updateAvatar);
+router.get('/', getUsers);
+router.get('/:userId', getUserById);
+router.post('/', createUser);
+router.patch('/me', updateUser);
+router.patch('/me/avatar', updateAvatar);
 
 module.exports = router;
