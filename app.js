157e9dc5df16adeee059566535da03d20974c12b
Gormsdottir <uv_hist@mail.ru>
2022-06-08 22:17:13 +0300

    ///

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const usersRoutes = require('./routes/users');
-const cardsRoutes = require('./routes/cards');
+
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
 
 app.use((req, res, next) => {
   req.user = {
@@ -23,8 +25,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/', usersRoutes);
-app.use('/', cardsRoutes);
-app.use((req, res) => res.status(404).send({ message: 'Невозможно отобразить страницу' }));
-
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`)
+})
