dcb1330b2be51f2877210818d381757e305894e8
Gormsdottir <61313338+Gormsdottir@users.noreply.github.com>
2022-06-09 20:58:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>404! Page not found</h1>');
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!")
 });
 
 app.listen(PORT, () => {
