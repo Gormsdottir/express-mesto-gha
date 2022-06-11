3cec2e190dfcf4e299f9ff8994cda2a770e023ab
Gormsdottir <uv_hist@mail.ru>
2022-06-12 00:18:37 +0300

    upd

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,7 @@
 const cardRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth');
-const { createCardValid } = require('../middlewares/validation');
+// const { createCardValid } = require('../middlewares/validation');
 
 const {
   getCards,
@@ -10,10 +11,38 @@ const {
   dislikeCard,
 } = require('../controllers/cards');
 
+// cardRouter.get('/', auth, getCards);
+// cardRouter.post('/', auth, createCardValid, createCard);
+// cardRouter.put('/:cardId/likes', auth, likeCard);
+// cardRouter.delete('/:cardId', auth, deleteCard);
+// cardRouter.delete('/:cardId/likes', auth, dislikeCard);
+
 cardRouter.get('/', auth, getCards);
-cardRouter.post('/', auth, createCardValid, createCard);
-cardRouter.put('/:cardId/likes', auth, likeCard);
-cardRouter.delete('/:cardId', auth, deleteCard);
-cardRouter.delete('/:cardId/likes', auth, dislikeCard);
+
+cardRouter.post('/', auth, celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().regex(/^(http(s)?:\/\/)(?:www\.|(?!www))+[\W\-._~:/?#[\]@!$&'()*+,;=]+#?$/),
+  }),
+}), createCard);
+
+cardRouter.delete('/:cardId', auth, celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), deleteCard);
+
+cardRouter.put('/:cardId/likes', auth, celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+
+}), likeCard);
+
+cardRouter.delete('/:cardId/likes', auth, celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), dislikeCard);
 
 module.exports = cardRouter;
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 const usersRouter = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const auth = require('../middlewares/auth');
-const {
-  userAvatarValid,
-  loginValid,
-  registerValid,
-} = require('../middlewares/validation');
+// const {
+//   userAvatarValid,
+//   loginValid,
+//   registerValid,
+// } = require('../middlewares/validation');
 
 const {
   getUsers,
@@ -16,12 +17,52 @@ const {
   login,
 } = require('../controllers/users');
 
+// usersRouter.get('/', auth, getUsers);
+// usersRouter.get('/:userId', auth, getUserById);
+// usersRouter.get('/me', auth, getUserMe);
+// usersRouter.post('/signup', registerValid, createUser);
+// usersRouter.patch('/me', auth, userAvatarValid, updateUserInfo);
+// usersRouter.patch('/me/avatar', userAvatarValid, updateUserAvatar);
+// usersRouter.post('/signin', loginValid, login);
+
 usersRouter.get('/', auth, getUsers);
-usersRouter.get('/:userId', auth, getUserById);
+
 usersRouter.get('/me', auth, getUserMe);
-usersRouter.post('/signup', registerValid, createUser);
-usersRouter.patch('/me', auth, userAvatarValid, updateUserInfo);
-usersRouter.patch('/me/avatar', userAvatarValid, updateUserAvatar);
-usersRouter.post('/signin', loginValid, login);
+
+usersRouter.get('/:userId', auth, celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().hex().length(24),
+  }),
+}), getUserById);
+
+usersRouter.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.,~#?&//=!]*$)/),
+  }),
+}), createUser);
+
+usersRouter.patch('/me', auth, celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2),
+  }),
+}), updateUserInfo);
+
+usersRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().regex(/^(http(s)?:\/\/)(?:www\.|(?!www))+[\W\-._~:/?#[\]@!$&'()*+,;=]+#?$/),
+  }),
+}), updateUserAvatar);
+
+usersRouter.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
 
 module.exports = usersRouter;
