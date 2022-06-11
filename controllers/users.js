032c89a6e93df207a7792b573d789317dbd1f703
Gormsdottir <61313338+Gormsdottir@users.noreply.github.com>
2022-06-11 21:07:41 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -7,87 +7,102 @@ const WrongDataError = require('../errors/WrongDataError');
 const AuthError = require('../errors/AuthError');
 const DuplicatedError = require('../errors/DuplicatedError');
 
-const getUsers = (req, res, next) => {
+const getUser = (req, res, next) => {
   User.find({})
-    .then((user) => {
-      res.status(200).send(user);
-    })
-    .catch(next);
+    .then((user) => res.status(200).send(user))
+    .catch((err) => next(err));
 };
-
-const getUserById = (req, res) => User.findById(req.params.userId)
-  .then((user) => {
-    if (!user) {
-      throw new NotFoundError('Пользователь не найден');
-    } else res.send({ user });
-  })
-  .catch((err) => {
-    if (err.name === 'CastError') {
-      res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
-    } else res.status(500).send({ message: 'Ошибка' });
-  });
 
 const getUserMe = (req, res, next) => {
   User.findById(req.user._id)
     .then((user) => {
-      res.status(200).send({ data: user });
+      if (!user._id) {
+        next(new NotFoundError('Пользователь не найден'));
+      }
+      res.status(200).send(user);
     })
-    .catch(next);
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new WrongDataError('Переданы некорректные данные.'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+const getUserById = (req, res, next) => {
+  User.findById(req.params.userId)
+    .orFail(() => {
+      throw new NotFoundError('Пользователь не найден');
+    })
+    .then((user) => {
+      if (!user._id) {
+        next(new NotFoundError('Пользователь не найден'));
+      }
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        next(new WrongDataError('Переданы некорректные данные.'));
+      } else {
+        next(err);
+      }
+    });
 };
 
 const createUser = (req, res, next) => {
   const {
     name,
-    avatar,
     about,
-    password,
+    avatar,
     email,
+    password,
   } = req.body;
 
-  return bcrypt.hash(password, 10).then((hash) => User.create({
-    name,
-    about,
-    avatar,
-    email,
-    password: hash,
-  }).then(() => res.send({
-    data: {
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        next(new DuplicatedError(`Пользователь с таким email ${email} уже зарегистрирован`));
+      }
+      return bcrypt.hash(password, 10);
+    })
+    .then((hash) => User.create({
       name,
       about,
       avatar,
       email,
-    },
-  })))
+      password: hash,
+    }))
+    .then((user) => User.findOne({ _id: user._id })) // прячет пароль
+    .then((user) => {
+      res.status(200).send(user);
+    })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        next(new WrongDataError('Неверные данные'));
-        return;
+        next(new WrongDataError('Переданы некорректные данные.'));
+      } else if (err.code === 11000) {
+        next(new DuplicatedError({ message: err.errorMessage }));
+      } else {
+        next(err);
       }
-      if (err.code === 11000) {
-        next(new DuplicatedError('Такой пользователь уже существует'));
-        return;
-      }
-      next(err);
     });
 };
 
 const updateUserInfo = (req, res, next) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, {
-    new: true,
-    runValidators: true,
-    upsert: false,
-  })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+    .orFail(() => {
+      throw new WrongDataError('Переданы некорректные данные');
+    })
     .then((user) => {
       if (!user) {
-        throw new NotFoundError('Такой пользователь не найден');
+        next(new WrongDataError('Переданы некорректные данные'));
       }
-      res.send({ data: user });
+      res.status(200).send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        next(new WrongDataError('Неверные данные'));
-        return;
+        next(new WrongDataError('Переданы некорректные данные'));
       }
       next(err);
     });
@@ -95,21 +110,19 @@ const updateUserInfo = (req, res, next) => {
 
 const updateUserAvatar = (req, res, next) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, {
-    new: true,
-    runValidators: true,
-    upsert: false,
-  })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+    .orFail(() => {
+      throw new WrongDataError('Переданы некорректные данные');
+    })
     .then((user) => {
       if (!user) {
-        throw new NotFoundError('Такой пользователь не найден');
+        next(new WrongDataError('Переданы некорректные данные'));
       }
-      res.send({ data: user });
+      res.status(200).send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        next(new WrongDataError('Неверные данные'));
-        return;
+        next(new WrongDataError('Переданы некорректные данные'));
       }
       next(err);
     });
@@ -117,23 +130,26 @@ const updateUserAvatar = (req, res, next) => {
 
 const login = (req, res, next) => {
   const { email, password } = req.body;
-
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
-
-      res.send({ token });
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        sameSite: true,
+      });
+      res.status(201).send({ message: 'Авторизация успешна', token });
     })
     .catch((err) => {
-      if (err.name === 'Error') {
-        next(new AuthError('Указан неверный e-mail или пароль'));
+      if (err.message === 'IncorrectEmail') {
+        next(new AuthError('Не правильный логин или пароль'));
       }
       next(err);
     });
 };
 
 module.exports = {
-  getUsers,
+  getUser,
   getUserById,
   getUserMe,
   createUser,
