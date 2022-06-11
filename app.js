2fd55387a393b2ea0fac7abc5d07755d3bfce401
Gormsdottir <uv_hist@mail.ru>
2022-06-11 21:32:35 +0300

    upd

--- app.js
+++ app.js
@@ -1,50 +1,27 @@
-/* eslint-disable no-console */
 const express = require('express');
+const { errors } = require('celebrate');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
-
-const NotFoundError = require('./errors/NotFoundError');
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
-
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const { registerValid, loginValid } = require('./middlewares/validation');
 
 const { PORT = 3000 } = process.env;
+
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+app.post('/signup', registerValid, createUser);
+app.post('/signin', loginValid, login);
 
-app.post('/signin', login);
+mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
 
-app.post('/signup', createUser);
+app.use('/cards', require('./routes/cards'));
 
 app.use(auth);
 
-app.use('/', usersRouter);
-app.use('/cards', cardsRouter);
-
 app.use(errors());
 
-app.use('/', (req, res, next) => {
-  next(new NotFoundError('Страница не существует'));
-});
-
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? 'Ошибка сервера'
-        : message,
-    });
-  next();
-});
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
+app.listen(PORT);
