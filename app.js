9446aab167f7323aaa65dbe80f386c90596f3311
Gormsdottir <61313338+Gormsdottir@users.noreply.github.com>
2022-06-12 15:44:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const usersRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
 const { login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const PageNotFound = require('./errors/PageNotFound');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -24,14 +25,12 @@ app.post('/signup', usersRouter);
 
 app.use(auth);
 
-app.use('/', usersRouter);
+app.use('/users', usersRouter);
 app.use('/cards', cardRouter);
 
 app.use(errors());
 
-app.use((req, res) => {
-  res.status(404).send({ message: 'Такой страницы нет' });
-});
+app.use('*', auth, PageNotFound);
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
@@ -42,7 +41,7 @@ app.use((err, req, res, next) => {
         : message,
     });
   next();
-});
+}); 
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
