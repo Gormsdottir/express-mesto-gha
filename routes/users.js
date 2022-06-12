d964c32c3bb248b94f5b77dae19efa20fdb0b45b
Valeriya Uldanova <uv_hist@mail.ru>
2022-06-12 16:57:19 +0300

    users fix

--- routes/users.js
+++ routes/users.js
@@ -34,7 +34,7 @@ usersRouter.post('/signup', celebrate({
 usersRouter.patch('/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2),
+    about: Joi.string().required().min(2).max(30),
   }),
 }), updateUserInfo);
 
