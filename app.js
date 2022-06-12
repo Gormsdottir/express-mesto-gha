9ef06a7a3fa1c06f3cb2c69997a7200a33ba12d3
Gormsdottir <61313338+Gormsdottir@users.noreply.github.com>
2022-06-12 13:14:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,26 +4,30 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 
-const auth = require('./middlewares/auth');
-const userRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
 const cardRouter = require('./routes/cards');
+const { login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
-const app = express();
 const { PORT = 3000 } = process.env;
-
-mongoose.connect('mongodb://localhost:27017/mestodb');
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', userRouter);
-app.use('/', cardRouter);
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
+app.post('/signin', login);
+app.post('/signup', usersRouter);
+
+app.use(auth);
+
+app.use('/users', usersRouter);
+app.use('/cards', cardRouter);
 
 app.use(errors());
-
-app.use((req, res) => {
-  res.status(404).send({ message: 'Такой страницы нет' });
-});
 
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
