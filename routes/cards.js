5f84b9c5ef479fe10bc157489337d987e7ea0965
Gormsdottir <61313338+Gormsdottir@users.noreply.github.com>
2022-06-12 15:33:08 +0300

    Update cards.js

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,6 @@
-const router = require('express').Router();
+const cardRouter = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const auth = require('../middlewares/auth');
+
 const {
   getCards,
   createCard,
@@ -9,47 +9,32 @@ const {
   dislikeCard,
 } = require('../controllers/cards');
 
-router.get('/cards', auth, getCards);
-router.post(
-  '/cards',
-  auth,
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      link: Joi.string().regex(/^(http(s)?:\/\/)(?:www\.|(?!www))+[\W\-._~:/?#[\]@!$&'()*+,;=]+#?$/),
-    }),
-  }),
-  createCard,
-);
-router.delete(
-  '/cards/:cardId',
-  auth,
-  celebrate({
-    params: Joi.object().keys({
-      cardId: Joi.string().hex().length(24),
-    }),
-  }),
-  deleteCard,
-);
-router.put(
-  '/cards/:cardId/likes',
-  auth,
-  celebrate({
-    params: Joi.object().keys({
-      cardId: Joi.string().hex().length(24),
-    }),
-  }),
-  likeCard,
-);
-router.delete(
-  '/cards/:cardId/likes',
-  auth,
-  celebrate({
-    params: Joi.object().keys({
-      cardId: Joi.string().hex().length(24),
-    }),
-  }),
-  dislikeCard,
-);
+cardRouter.get('/', getCards);
 
-module.exports = router;
+cardRouter.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().regex(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.,~#?&//=!]*$)/),
+  }),
+}), createCard);
+
+cardRouter.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), deleteCard);
+
+cardRouter.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+
+}), likeCard);
+
+cardRouter.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), dislikeCard);
+
+module.exports = cardRouter;
