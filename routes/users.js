ce2f4b4691812001c3d7fac40791234e5f98b216
Gormsdottir <61313338+Gormsdottir@users.noreply.github.com>
2022-06-12 13:23:30 +0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
-const router = require('express').Router();
+const usersRouter = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const auth = require('../middlewares/auth');
+
 const {
   getUsers,
   getUserById,
@@ -11,63 +11,44 @@ const {
   login,
 } = require('../controllers/users');
 
-router.post(
-  '/signup',
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().regex(/^(http(s)?:\/\/)(?:www\.|(?!www))+[\W\-._~:/?#[\]@!$&'()*+,;=]+#?$/),
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  createUser,
-);
-router.post(
-  '/signin',
-  celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  login,
-);
+usersRouter.get('/', getUsers);
 
-router.get('/users', auth, getUsers);
+usersRouter.get('/me', getUserMe);
 
-router.get('/users/me', auth, getUserMe);
-router.get(
-  '/users/:id',
-  auth,
-  celebrate({
-    params: Joi.object().keys({
-      id: Joi.string().hex().length(24),
-    }),
+usersRouter.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().hex().length(24),
   }),
-  getUserById,
-);
-router.patch(
-  '/users/me',
-  auth,
-  celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-    }),
-  }),
-  updateUserInfo,
-);
-router.patch(
-  '/users/me/avatar',
-  auth,
-  celebrate({
-    body: Joi.object().keys({
-      avatar: Joi.string().regex(/^(http(s)?:\/\/)(?:www\.|(?!www))+[\W\-._~:/?#[\]@!$&'()*+,;=]+#?$/),
-    }),
-  }),
-  updateUserAvatar,
-);
+}), getUserById);
 
-module.exports = router;
+usersRouter.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().regex(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.,~#?&//=!]*$)/),
+  }),
+}), createUser);
+
+usersRouter.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2),
+  }),
+}), updateUserInfo);
+
+usersRouter.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().regex(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.,~#?&//=!]*$)/),
+  }),
+}), updateUserAvatar);
+
+usersRouter.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+
+module.exports = usersRouter;
